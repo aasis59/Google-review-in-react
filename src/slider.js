fa9c957741658e0dec33b20ae006a790168711ef
aasis59 <aasis452711@gmail.com>
2023-09-19 12:38:02 +0545

    json and api added

--- src/slider.js
+++ src/slider.js
@@ -3,6 +3,7 @@ import './home.scss';
 
 import { Swiper, SwiperSlide } from 'swiper/react';
 import Slide from './slide';
+import './slider.scss';
 
 
 // Import Swiper styles
@@ -13,13 +14,21 @@ import 'swiper/css/scrollbar';
 
 export default  () => {
   return (
+  <div className='swiper-section'>
+  <div className="swiper-button-prev swiper2-prev"></div>
     <Swiper
       // install Swiper modules
       modules={[Navigation, Pagination, Scrollbar, A11y]}
       spaceBetween={20}
       slidesPerView={3}
-      navigation
-      pagination={{ clickable: true }}
+      navigation={{clickable: true,
+        nextEl: ".swiper2-next",
+        prevEl: ".swiper2-prev"}}
+        pagination={{
+          clickable: true,
+          el: ".swiper2-page",
+      }}
+      
         
       onSwiper={(swiper) => console.log(swiper)}
       onSlideChange={() => console.log('slide change')}
@@ -35,5 +44,9 @@ export default  () => {
 
       
     </Swiper>
+  <div className="swiper-button-next swiper2-next"></div>
+           
+    <div className="swiper-pagination swiper2-page"></div>
+    </div>
   );
 };
